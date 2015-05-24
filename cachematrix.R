5134697bea6478f98b626b7c2ef6d923954c108e
Martin Jenkins <martin@gyromancy.com.au>
2015-05-24 08:08:55 +0000

    Implemented assignment requirements - cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Wraps a matrix in a way that its inverse can be cached
+## This contravenes the basic nature of abstraction that is fundamental to OOP,
+## but is appropriate for the assignment
 
-## Write a short comment describing this function
+## Makes a matrix wrapper with functions get, set, getsolution, setsolution
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolution <- function(solution) s <<- solution
+    getsolution <- function() s
+    list(set = set, get = get,
+         setsolution = setsolution,
+         getsolution = getsolution)
 }
 
 
-## Write a short comment describing this function
+## Returns the cached solution if available, otherwise calculates, caches and
+## then returns the result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolution()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolution(s)
+    s
 }
